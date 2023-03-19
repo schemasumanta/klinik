fd7a30bf700406d8dfe0a3447b15e8dfa1fa5f8b
schemasumanta <107915904+schemasumanta@users.noreply.github.com>
2023-03-20 00:36:50 +0700

    dashboard setting

--- application/views/tampilan_dashboard.php
+++ application/views/tampilan_dashboard.php
@@ -5,7 +5,7 @@
 				<div class="d-flex align-items-left align-items-md-center flex-column flex-md-row">
 					<div>
 
-						<h2 class="text-white pb-2 fw-bold">Dashboard 2</h2>
+						<h2 class="text-white pb-2 fw-bold">Dashboard</h2>
 						<h2 class="text-white op-7 mb-2">Selamat Datang <?php echo  $this->session->nama_admin; ?>
 
 						</h2>
